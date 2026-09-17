1b909b3d5f9340f84a6a884264839ef67a4f43f5
ganesh-k17 <ganku007@live.in>
2026-09-17 19:59:23 +0530

    feat: adds jenkins file

--- test_prediction.py
+++ test_prediction.py
@@ -1,6 +1,9 @@
+import os
 import requests
 
-url = "https://house-model-api.onrender.com/predict" # site is deployed in render.com
+# url = "https://house-model-api.onrender.com/predict" # site is deployed in render.com
+url = os.getenv("API_URL", "http://127.0.0.1:5000/predict")
+
 
 data = {
     "size": 2000
